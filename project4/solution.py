2bc726918453d7f83192ca9e3078051d65f2a4cd
Peizhe Zhang <zjuzpz@gmail.com>
2016-03-17 14:55:13 -0700

    Added files via upload

---
+++ project4/solution.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 16 22:14:14 2016
+
+@author: Peizhe
+"""
+from problem1 import solution1
+from problem2 import solution2
+
+def solution():
+    solution1()
+    solution2()
+    
+solution()
