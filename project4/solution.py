115f48ff13868a211a091a4845153b358d93912c
Peizhe Zhang <zjuzpz@gmail.com>
2016-03-18 23:43:11 -0700

    Added files via upload

---
+++ project4/solution.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 16 22:14:14 2016
+
+@author: Peizhe
+"""
+from problem1 import solution1
+from problem2 import solution2
+from problem3 import solution3
+from problem4 import solution4
+from problem5 import solution5
+from problem6 import solution6
+
+def solution():
+    solution1()
+    solution2()
+    solution3()
+    solution4()
+    solution5()
+    solution6()
+    
+solution()
